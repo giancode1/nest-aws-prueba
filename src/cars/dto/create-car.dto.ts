ecfa4a38862b239b255699da8133436fdc60f339
Giancarlo Culcay <gieg5c@gmail.com>
2022-12-04 17:31:35 -0500

    Pipes Globales - A nivel de Aplicación

--- src/cars/dto/create-car.dto.ts
+++ src/cars/dto/create-car.dto.ts
@@ -1,13 +1,14 @@
-import { IsString } from "class-validator";
+import { IsString, MinLength } from "class-validator";
 
 // los DTOs siempre son clases
 export class CreateCarDto {
 
-    // @IsString({ message: `mi mensaje personalizado`})
+    // @IsString({ message: `mi mensaje personalizado`})  //realmente puedes personalizar cualquier decorador
     @IsString()
     readonly brand: string;
     
     @IsString()
+    @MinLength(3)
     readonly model: string;
 }
 
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(  // GlobalPipes
+    new ValidationPipe({
+      whitelist: true,  // solo deja pasar la data que estoy esperando, elimina data que no es
+      forbidNonWhitelisted: true, // ahora me dice que tales campos no deben ser enviados
+    })
+  )
+
+
   await app.listen(3000);
 }
 bootstrap();
+
+// no olvidar instalar: pnpm i class-validator class-transformer
 
 // puedes agregar un pipe a nivel global de aplicación
 // puedes poner middlewares y demas a diferentes nivel depende de lo que necesites
