35cd3ba182c89a15803e927854536095a06299eb
Giancarlo Culcay <giancode@outlook.com>
2022-12-09 02:00:59 -0500

    Ruta /

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { CarsModule } from './cars/cars.module';
 import { BrandsModule } from './brands/brands.module';
 import { SeedModule } from './seed/seed.module';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 // los decoradores convierten las clases en objetos con funcionalidad especifica
 
 @Module({   // esto es un @decorador
   imports: [CarsModule, BrandsModule, SeedModule],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService],
   exports: [],
 })
 export class AppModule {} //es una simple clase con el decorador Module
